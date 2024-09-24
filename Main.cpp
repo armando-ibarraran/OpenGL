4f11734487c3b2fe5e26cf752d528cc0c5067d9a
Armando Ibarrarán <46429550+AI-2003@users.noreply.github.com>
2024-09-23 18:04:18 -0600

    Clase18Sep2024

--- Main.cpp
+++ Main.cpp
@@ -1,304 +1,218 @@
-#include <stdio.h>
-#include <stdlib.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
 
-#define MAX_STRING_SIZE 50  // Para estandarizar la longitud de los strings
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-// Estructuras usadas para almacenar la info
-typedef struct {
-    char calle[MAX_STRING_SIZE];
-    int numero_externo;
-    char codigo_postal[MAX_STRING_SIZE];
-    char ciudad[MAX_STRING_SIZE];
-    char pais[MAX_STRING_SIZE];
-} Direccion;
+#include <shader.h>
+#include <shader_m.h>
+#include <shader_s.h>
 
-typedef struct Empleado {
-    char nombres[MAX_STRING_SIZE];
-    char apellidos[MAX_STRING_SIZE];
-    int numero_ingreso;
-    Direccion direccion;
-    float salario;
-    char genero;
-    char estado_civil;
-    int numero_hijos;
-    struct Empleado* siguiente;
-} Empleado;
+#include <iostream>
 
-// Definición temporal de funciones
-Empleado* crear_empleado();
-int insertar_empleado(Empleado** lista);
-int borrar_empleado(Empleado** lista, int numero_ingreso);
-int num_empleados(Empleado* lista);
-Empleado* buscar_empleado(Empleado* lista, int numero_ingreso);
-void mostrar_empleado(Empleado* empleado);
-void los_empleados(Empleado* lista);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-int main() {
-    Empleado* lista = NULL; // Inicializa la lista de empleados como vacía.
-    int opcion, numero_ingreso;
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-    // Bucle principal del programa para gestionar las opciones del usuario.
-    do {
-        printf("\nPrograma de nómina\n");
-        printf("1. Insertar empleado\n");
-        printf("2. Borrar empleado\n");
-        printf("3. Mostrar un empleado\n");
-        printf("4. Mostrar todos los empleados\n");
-        printf("5. Número de empleados\n");
-        printf("6. Salir\n");
-        printf("Seleccione una opción: ");
-        scanf_s("%d", &opcion);
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        printf("\n");
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-        switch (opcion) {
-        case 1: {
-            if (insertar_empleado(&lista)) {
-                printf("Empleado insertado correctamente.\n");
-            }
-            else {
-                printf("Error al insertar empleado.\n");
-            }
-            break;
-        }
-        case 2: {
-            printf("Ingrese el número de ingreso del empleado a borrar: ");
-            scanf_s("%d", &numero_ingreso);
-            if (borrar_empleado(&lista, numero_ingreso)) {
-                printf("Empleado borrado correctamente.\n");
-            }
-            else {
-                printf("Empleado no encontrado.\n");
-            }
-            break;
-        }
-        case 3: {
-            printf("Ingrese el número de ingreso del empleado a mostrar: ");
-            scanf_s("%d", &numero_ingreso);
-            Empleado* emp = buscar_empleado(lista, numero_ingreso);
-            if (emp != NULL) {
-                mostrar_empleado(emp);
-            }
-            else {
-                printf("Empleado no encontrado.\n");
-            }
-            break;
-        }
-        case 4: {
-            los_empleados(lista); // Muestra la lista de todos los empleados.
-            break;
-        }
-        case 5: {
-            printf("Número de empleados: %d\n", num_empleados(lista)); // Muestra el número total de empleados.
-            break;
-        }
-        case 6: {
-            printf("Saliendo del programa.\n");
-            break;
-        }
-        default: {
-            printf("Opción no válida.\n");
-            break;
-        }
-        }
-    } while (opcion != 6);
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Transformaciones", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("5.1.transform.vs", "5.1.transform.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions          // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // load and create a texture 
+    // -------------------------
+    unsigned int texture1, texture2;
+    // texture 1
+    // ---------
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    unsigned char* data = stbi_load("C:/openGL/texturas/ny.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    // texture 2
+    // ---------
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    data = stbi_load("C:/openGL/texturas/creeper.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // create transformations
+        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+        // get matrix's uniform location and set matrix
+        ourShader.use();
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        // render container
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
     return 0;
 }
 
-/**
- * Función: crear_empleado
- * Descripción: Crea un nuevo empleado solicitando los datos al usuario y los almacena en una estructura.
- * Retorno: Un puntero al empleado recién creado o NULL si hubo un error de memoria.
- */
-Empleado* crear_empleado() {
-    Empleado* nuevo = (Empleado*)malloc(sizeof(Empleado)); // Asigna memoria para un nuevo empleado.
-    if (nuevo == NULL) {
-        printf("Error: No hay memoria suficiente para almacenar un nuevo empleado.\n");
-        return NULL;
-    }
-
-    // Solicita al usuario los datos del empleado.
-    printf("Ingrese los datos del empleado.\n");
-    printf("Nombres: ");
-    scanf_s(" %[^\n]", nuevo->nombres, (unsigned int)MAX_STRING_SIZE);
-    printf("Apellidos: ");
-    scanf_s(" %[^\n]", nuevo->apellidos, (unsigned int)MAX_STRING_SIZE);
-    printf("Número de ingreso: ");
-    scanf_s("%d", &nuevo->numero_ingreso);
-
-    // Captura los detalles de la dirección del empleado.
-    printf("Dirección: \n");
-    printf("\tCalle o avenida: ");
-    scanf_s(" %[^\n]", nuevo->direccion.calle, (unsigned int)MAX_STRING_SIZE);
-    printf("\tNúmero externo: ");
-    scanf_s("%d", &nuevo->direccion.numero_externo);
-    printf("\tCódigo postal: ");
-    scanf_s(" %[^\n]", nuevo->direccion.codigo_postal, (unsigned int)MAX_STRING_SIZE);
-    printf("\tCiudad: ");
-    scanf_s(" %[^\n]", nuevo->direccion.ciudad, (unsigned int)MAX_STRING_SIZE);
-    printf("\tPaís: ");
-    scanf_s(" %[^\n]", nuevo->direccion.pais, (unsigned int)MAX_STRING_SIZE);
-
-    printf("Salario: ");
-    scanf_s("%f", &nuevo->salario);
-    printf("Género (M/F): ");
-    scanf_s(" %c", &nuevo->genero, 1);
-    printf("Estado civil (S/C/D): ");
-    scanf_s(" %c", &nuevo->estado_civil, 1);
-    printf("Número de hijos: ");
-    scanf_s("%d", &nuevo->numero_hijos);
-
-    nuevo->siguiente = NULL; // El nuevo empleado apunta a NULL, ya que será el último en la lista.
-    return nuevo;
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
-/**
- * Función: insertar_empleado
- * Descripción: Inserta un nuevo empleado en la lista enlazada de empleados en orden creciente según su número de ingreso.
- * Parámetros:
- *      - lista: Un puntero doble a la lista de empleados.
- * Retorno: Devuelve 1 si la inserción fue exitosa, 0 si hubo un error de memoria.
- */
-int insertar_empleado(Empleado** lista) {
-    Empleado* nuevo = crear_empleado();
-    if (nuevo == NULL) {
-        return 0; // Error de memoria
-    }
-
-    if (*lista == NULL || (*lista)->numero_ingreso > nuevo->numero_ingreso) {
-        // Caso en que el empleado debe insertarse al inicio de la lista.
-        nuevo->siguiente = *lista;
-        *lista = nuevo;
-    }
-    else {
-        Empleado* actual = *lista;
-        // Itera para encontrar la posición adecuada del nuevo empleado en la lista.
-        while (actual->siguiente != NULL && actual->siguiente->numero_ingreso < nuevo->numero_ingreso) {
-            actual = actual->siguiente;
-        }
-        nuevo->siguiente = actual->siguiente;
-        actual->siguiente = nuevo;
-    }
-
-    return 1;
-}
-
-/**
- * Función: borrar_empleado
- * Descripción: Elimina un empleado de la lista enlazada según su número de ingreso.
- * Parámetros:
- *      - lista: Un puntero doble a la lista de empleados.
- *      - numero_ingreso: El número de ingreso del empleado a eliminar.
- * Retorno: Devuelve 1 si el empleado fue encontrado y eliminado, 0 si no se encontró.
- */
-int borrar_empleado(Empleado** lista, int numero_ingreso) {
-    if (*lista == NULL) {
-        return 0; // Lista vacía.
-    }
-
-    Empleado* actual = *lista;
-    Empleado* anterior = NULL;
-
-    // Itera sobre la lista para encontrar el empleado con el número de ingreso especificado.
-    while (actual != NULL && actual->numero_ingreso != numero_ingreso) {
-        anterior = actual;
-        actual = actual->siguiente;
-    }
-
-    // Si no se encuentra el empleado.
-    if (actual == NULL) {
-        return 0;
-    }
-
-    // Si el empleado a eliminar es el primero en la lista.
-    if (anterior == NULL) {
-        *lista = actual->siguiente;
-    }
-    else {
-        // Se salta el empleado a eliminar.
-        anterior->siguiente = actual->siguiente;
-    }
-
-    free(actual); // Libera la memoria ocupada por el empleado eliminado.
-    return 1;
-}
-
-/**
- * Función: num_empleados
- * Descripción: Cuenta el número total de empleados en la lista enlazada.
- * Parámetros:
- *      - lista: Un puntero a la lista de empleados.
- * Retorno: El número de empleados en la lista.
- */
-int num_empleados(Empleado* lista) {
-    int contador = 0;
-
-    // Recorre la lista y cuenta cada empleado.
-    while (lista != NULL) {
-        contador++;
-        lista = lista->siguiente;
-    }
-
-    return contador; // Devuelve el número total de empleados.
-}
-
-/**
- * Función: buscar_empleado
- * Descripción: Busca un empleado en la lista enlazada según su número de ingreso.
- * Parámetros:
- *      - lista: Un puntero a la lista de empleados.
- *      - numero_ingreso: El número de ingreso del empleado que se busca.
- * Retorno: Un puntero al empleado si se encuentra, o NULL si no se encuentra.
- */
-Empleado* buscar_empleado(Empleado* lista, int numero_ingreso) {
-    // Recorre la lista en busca del empleado con el número de ingreso especificado.
-    while (lista != NULL) {
-        if (lista->numero_ingreso == numero_ingreso) {
-            return lista; // Retorna el empleado si se encuentra.
-        }
-        lista = lista->siguiente;
-    }
-
-    return NULL; // Retorna NULL si no se encuentra el empleado.
-}
-
-/**
- * Función: mostrar_empleado
- * Descripción: Muestra la información de un empleado específico.
- * Parámetros:
- *      - empleado: Un puntero al empleado cuya información se va a mostrar.
- */
-void mostrar_empleado(Empleado* empleado) {
-    if (empleado != NULL) {
-        printf("\n--- Información del Empleado ---\n");
-        printf("Nombres: %s\n", empleado->nombres);
-        printf("Apellidos: %s\n", empleado->apellidos);
-        printf("Número de ingreso: %d\n", empleado->numero_ingreso);
-        printf("Dirección: %s, %d, %s, %s, %s\n", empleado->direccion.calle, empleado->direccion.numero_externo,
-            empleado->direccion.codigo_postal, empleado->direccion.ciudad, empleado->direccion.pais);
-        printf("Salario: %.2f\n", empleado->salario);
-        printf("Género: %c\n", empleado->genero);
-        printf("Estado civil: %c\n", empleado->estado_civil);
-        printf("Número de hijos: %d\n", empleado->numero_hijos);
-    }
-}
-
-/**
- * Función: los_empleados
- * Descripción: Muestra la información de todos los empleados en la lista enlazada.
- * Parámetros:
- *      - lista: Un puntero a la lista de empleados.
- */
-void los_empleados(Empleado* lista) {
-    if (lista == NULL) {
-        printf("No hay empleados en la lista.\n");
-    }
-    else {
-        // Recorre la lista mostrando cada empleado.
-        while (lista != NULL) {
-            mostrar_empleado(lista);
-            lista = lista->siguiente;
-        }
-    }
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
